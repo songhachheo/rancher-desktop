f3b78b43a92f1bae96feb87b17458d0c1cc75e53
Eric Promislow <epromislow@suse.com>
2022-06-23 10:43:24 -0700

    Add an empty credHelpers object for the list command.
    Signed-off-by: Eric Promislow <epromislow@suse.com>

--- src/main/credentialServer/httpCredentialHelperServer.ts
+++ src/main/credentialServer/httpCredentialHelperServer.ts
@@ -185,7 +185,7 @@ export class HttpCredentialHelperServer {
     response: http.ServerResponse): Promise<void> {
     try {
       const serverAndUsernameInfo: Record<string, string> = JSON.parse(await this.runCommand(`docker-credential-${ thisHelperInfo.credsStore }`, 'list', ''));
-      const names = _.uniq(Object.values(thisHelperInfo.credHelpers));
+      const names = _.uniq(Object.values(thisHelperInfo.credHelpers ?? {}));
 
       for (const name of names) {
         try {
